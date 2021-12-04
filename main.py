dac16e2c11c61f468d1e8a338c2c3bc2bb64de9f
Sofia <sofiateeriaho@mac.com>
2021-12-04 18:34:14 +0100

    Upper-conf-bound and normal distr. now correct

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+import math
 
 class Action:
     def __init__(self, id):
@@ -11,14 +12,32 @@ class Action:
         self.mean = 0
         # number of trials
         self.N = 0
+        # probability of success
+        self.p = 0
+        self.std = 1
 
     # initial reward average
     def start_reward(self, reward):
         self.mean = reward
 
+    # return reward from standard normal distribution
     # return chosen action
     def choose_action(self):
-        return np.random.randn() + self.id
+        ## this one has more control over variance and std
+        reward = np.random.normal(self.mean, self.std)
+        return np.round(reward, 1)
+
+        # randn gives a distribution from some standardized normal distribution (mean 0 and variance 1)
+        # reward = np.random.randn() + self.id
+        # return reward
+
+    def choose_action_bernoulli(self):
+        if random.random() > self.mean:
+            return 0.0
+        else:
+            return 1.0
+        # return np.random.binomial(1, self.p)
+        #return np.random.binomial(1, self.p, size=1)[0]
 
     # update the action-value estimate
     def update(self, x):
@@ -34,27 +53,24 @@ class Action:
 # Plot chosen actions and average trend of rewards over iterations(N)
 def plot_avg(actions, averages, title, k):
 
-    bins = np.arange(k+2) - 0.5
-    plt.hist(actions, bins, edgecolor='black')
-    plt.xticks(range(k+1))
-    plt.xlim([0, k+1])
-
-    plt.title('Histogram of actions selections')
-    plt.xlabel('Actions')
-    plt.ylabel('Number of times selected')
+    # plot moving average
+    plt.plot(averages)
+    plt.title(title)
+    plt.ylabel('Average reward value')
+    plt.xlabel('Iterations')
     axes = plt.gca()
     axes.yaxis.grid()
     plt.show()
 
-    # plot moving average
-    plt.plot(averages)
+    # plot histogram of chosen actions
+    bins = np.arange(k + 2) - 0.5
+    plt.hist(actions, bins, edgecolor='black')
+    plt.xticks(range(k + 1))
+    plt.xlim([0, k + 1])
 
-    # for c in range(1,k+1):
-    #     plt.plot(np.ones(N) * c)
-
-    plt.title(title)
-    plt.ylabel('Reward value')
-    plt.xlabel('Iterations')
+    plt.title('Histogram of actions selections')
+    plt.xlabel('Actions')
+    plt.ylabel('Number of times selected')
     axes = plt.gca()
     axes.yaxis.grid()
     plt.show()
@@ -117,6 +133,7 @@ def greedy(k, N):
     plot_avg(chosen_actions, avg, "Greedy Method", k)
 
 def optimistic_initial_values(eps, start, k, N):
+
     # initialize actions
     actions = []
     for a in range(1, k + 1):
@@ -151,35 +168,53 @@ def optimistic_initial_values(eps, start, k, N):
     avg = np.cumsum(chosen_rewards) / (np.arange(N) + 1)
     plot_avg(chosen_actions, avg, "Optimistic Initial Values Method", k)
 
-def upper_conf_bound(eps, conf, k, N):
+def upper_conf_bound(k, N):
 
     # initialize actions
     actions = []
     for a in range(1, k + 1):
         actions.append(Action(a))
 
-    # choose a percentage of indexes given epsilon
-    random_list = []
-    nr = int(eps * float(N))
-    for i in range(nr):
-        random_list.append(random.randint(0, N))
+    # keep track of chosen actions and their rewards
+    chosen_actions = []
+    chosen_rewards = np.empty(N)
 
-    # keep track of chosen action to calculate total reward
-    chosen_actions = np.empty(N)
+    # how many times said action is chosen
 
-    # implement rest
-    print("Not implemented yet")
+    sums_of_reward = [0] * k
+    total_reward = 0
+
+    for n in range(0, N):
+        arm = 0
+        max_upper_bound = 0
+        for i in range(0, k):
+            if (actions[i].N > 0):
+                average_reward = sums_of_reward[i] / actions[i].N
+                c = math.sqrt(2 * math.log(n + 1) / actions[i].N)
+                upper_bound = average_reward + c
+            else:
+                upper_bound = 1e400
+
+            if upper_bound > max_upper_bound:
+                max_upper_bound = upper_bound
+                arm = i
+
+        reward = actions[arm].choose_action()
+        actions[arm].update(reward)
+        chosen_rewards[n] = reward
+        sums_of_reward[arm] += reward
+        total_reward += reward
+        chosen_actions.append(actions[arm].id)
+
+    avg = np.cumsum(chosen_rewards) / (np.arange(N) + 1)
+    plot_avg(chosen_actions, avg, "Upper-Confidence Bound Method", k)
 
 def print_methods():
     print(f'Choose a method: \n 1 - Greedy \n 2 - Epsilon-greedy '
           f'\n 3 - Optimistic initial values \n 4 - Softmax policy '
           f'\n 5 - Upper-Confidence Bound \n 6 - Action Preferences')  # Press ⌘F8 to toggle the breakpoint.
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-
-    print_methods()
-
+def user_interact():
     # ask user for method
     m = int(input('Option: - '))
 
@@ -204,16 +239,21 @@ if __name__ == '__main__':
 
     elif m == 4:
         print('Not implemented')
-        #Softmax policy
+        # Softmax policy
 
     elif m == 5:
         print('Please specify the following parameters')
-        eps = float(input('epsilon: - '))
-        conf = int(input('confidence bound: - '))
         k = int(input('k: - '))
         N = int(input('N: - '))
-        upper_conf_bound(eps, conf, k, N)
+        upper_conf_bound(k, N)
 
     elif m == 6:
         print('Not implemented')
-        #Action preferences
+        # Action preferences
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    print_methods()
+    user_interact()
+
